e78e3a8e4f1c2339f44f0a5faa2a1b0b17fb2c4f
Diogenes Brito <uxdiogenes@users.noreply.github.com>
2026-05-04 21:56:06 -0400

    feat: add public exports (index.ts)

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// src/index.ts
+export {
+  metrcTransferSchema, metrcPackageSchema, metrcDeliverySchema,
+  deliveryWithPackagesSchema,
+} from "./schemas/index.js";
+
+export type {
+  MetrcTransfer, MetrcPackage, MetrcDelivery, DeliveryWithPackages,
+} from "./schemas/index.js";
+
+export type { MetrcClient, MetrcConfig } from "./client/interface.js";
+export type { Logger } from "./logger.js";
+export { NOOP_LOGGER } from "./logger.js";
+export type { RetryConfig } from "./transport/retry.js";
+
+export { createLiveMetrcClient } from "./client/live.js";
+export { createMockMetrcClient, DEFAULT_MOCK_FIXTURES } from "./client/mock.js";
+export type { MockFixtures } from "./client/mock.js";
+
+export { NY_PROD_BASE_URL, NY_SANDBOX_BASE_URL } from "./constants.js";
+
+export {
+  MetrcError, MetrcAuthError, MetrcClientError, MetrcRateLimitError,
+  MetrcServerError, MetrcNetworkError, MetrcResponseError,
+} from "./errors.js";
